577237e619003e3c9ec2d710a723cff5cebe96c4
Bogdan <b.aksenov@yadro.com>
2024-09-06 11:47:32 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,24 @@ export default defineConfig({
   plugins: [react(), svgr(), tsconfigPaths()],
   server: {
     port: 5174,
+    host: true,
+  },
+  build: {
+    outDir: "build",
+    assetsDir: "static",
+    rollupOptions: {
+      input: {
+        app: "./index.html",
+      },
+      output: {
+        manualChunks: (module: string) => {
+          // @ts-ignore
+          if (module.includes("node_modules")) {
+            return "npm-packages"
+          }
+          return null
+        },
+      },
+    },
   },
 })
